c887f29950628f508090d5a655b55d67adea0811
MatheusSouza2401 <84870432+MatheusSouza2401@users.noreply.github.com>
2022-02-07 20:19:45 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Rede MAIS Salvação</title>
+  <link href="_css/style.css" rel="stylesheet" />
+  <script src="js/jquery.js"></script>
+  <script src="js/function.js"></script>
+</head>
+<body>
+    <header>
+        <div class="menu-desktop">
+          <div class="logomarca">
+              <img src="_imagem/bandeira-rede-mais.gif" />
+          </div><!--logomarca-->
+          <div class="center-1">
+              <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">SOBRE NÓS</a></li>
+                <li><a href="#">NOSSO TRABALHO</a></li>
+                <li><a href="#">CONTATO</a></li>
+              </ul>
+          </div>
+        </div><!--menu-desktop-->
+        <div class="menu-mobile">
+          <div class="logomarca">
+              <img src="_imagem/bandeira-rede-mais.gif" />
+          </div><!--logomarca-->
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">SOBRE NÓS</a></li>
+                <li><a href="#">NOSSO TRABALHO</a></li>
+                <li><a href="#">CONTATO</a></li>
+              </ul>
+        </div><!--menu-mobile-->
+        <div class="apoio">
+            <div class="borda">
+              <p> APOIE ESSE PROJETO </p>
+            </div><!--borda-->
+        </div><!--apoio-->
+    </header>
+    <div class="banner">
+       <video autoplay muted loop>
+            <source src="_midia/video-banner.mp4" type="video/mp4" />
+       </video>
+
+       <div class="textos-banner">
+          <h1> REDE MAIS SALVAÇÃO</h1>
+       </div><!--textos-banner-->
+    </div><!--banner-->
+</body>
+</html>
